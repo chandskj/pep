48d760cf5cf0784e6b6ba1f61c4d475367614cd9
Sai Chand <saichand.kolloju@gmail.com>
2019-03-05 22:03:21 +0530

    Created Maven Project

---
+++ src/main/java/com/gid/aid/App.java
@@ -0,0 +1,13 @@
+package com.gid.aid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
